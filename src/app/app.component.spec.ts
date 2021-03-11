d0f3cd07ed32ccb64bb08218ddd2dfd9b9c2ad81
PierreDemailly <pierredemailly.pro@gmail.com>
2021-03-11 17:49:20 +0100

    make linter to pass

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-electron-boilerplate app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+        'angular-electron-boilerplate app is running!',
+    );
   });
 });
