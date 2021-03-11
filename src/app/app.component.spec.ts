3a454be0d9a1cc61a9d85a2b8d2450d527f47453
PierreDemailly <pierredemailly.pro@gmail.com>
2021-03-11 22:09:17 +0100

    remove useless code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,19 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'angular-electron-boilerplate'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-electron-boilerplate');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-        'angular-electron-boilerplate app is running!',
-    );
-  });
 });
